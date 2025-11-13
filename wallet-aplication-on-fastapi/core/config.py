7d49fb3ef2cb8cd505cd219c030c267a1d31b91a
Leshik1305 <realleshik1305@gmail.com>
2025-11-13 21:14:13 +0300

    update settings

--- wallet-aplication-on-fastapi/core/config.py
+++ wallet-aplication-on-fastapi/core/config.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, PostgresDsn
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
 
 class RunConfig(BaseModel):
     host: str = "127.0.0.1"
@@ -21,6 +22,12 @@ class DatabaseConfig(BaseModel):
 class Settings(BaseSettings):
     title: str = "FastAPI Wallet"
     version: str = "0.1.0"
+    model_config = SettingsConfigDict(
+        env_file=(".env.template", ".env"),
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        env_prefix="APP_CONFIG__",
+    )
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
     db: DatabaseConfig
