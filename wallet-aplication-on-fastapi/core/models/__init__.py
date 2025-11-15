730df0deb9bf07df1f11564edf6b42aca4f131e1
Leshik1305 <realleshik1305@gmail.com>
2025-11-15 15:15:02 +0300

    create wallet model

--- wallet-aplication-on-fastapi/core/models/__init__.py
+++ wallet-aplication-on-fastapi/core/models/__init__.py
@@ -1,7 +1,9 @@
 __all__ = (
     "db_helper",
     "Base",
+    "Wallet",
 )
 
 from .db_helper import db_helper
 from .base import Base
+from .wallet import Wallet
---
+++ wallet-aplication-on-fastapi/core/models/wallet.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+
+from sqlalchemy import Boolean, Numeric, DateTime, func
+from sqlalchemy.orm import Mapped, mapped_column
+
+from . import Base
+from .mixins.created_at import CreatedAtMixin
+from .mixins.uuid_pk import IdUuidPkMixin
+
+
+class Wallet(IdUuidPkMixin, CreatedAtMixin, Base):
+    balance: Mapped[float] = mapped_column(
+        Numeric(10, 2),
+    )
+    updated: Mapped[datetime | None] = mapped_column(
+        DateTime(),
+        nullable=True,
+        default=None,
+        server_default=None
+    )
+    is_active: Mapped[bool] = mapped_column(
+        Boolean,
+        default=True,
+    )
+
+    def __repr__(self):
+        return f"Wallet - {self.id}. Balance - {self.balance}"
