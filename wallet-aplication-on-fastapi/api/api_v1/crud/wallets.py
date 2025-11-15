350aff5c5ce7368b71a0995d36e9d0334b919937
Leshik1305 <realleshik1305@gmail.com>
2025-11-15 20:21:39 +0300

    create change balance def

--- wallet-aplication-on-fastapi/api/api_v1/crud/wallets.py
+++ wallet-aplication-on-fastapi/api/api_v1/crud/wallets.py
@@ -1,11 +1,11 @@
 from uuid import UUID
 
 from fastapi import HTTPException, status
-from sqlalchemy import select
+from sqlalchemy import select, update
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.models import Wallet
-from core.schemas.wallet import WalletCreate
+from core.schemas.wallet import WalletCreate, WalletUpdate, OperationEnum
 
 
 async def create_wallet(
@@ -30,4 +30,40 @@ async def get_wallet(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="Кошелек по данному ID не найден",
         )
+    return wallet
+
+
+async def change_balance(
+    session: AsyncSession,
+    id: UUID,
+    update_wallet: WalletUpdate,
+) -> Wallet:
+    wallet = await get_wallet(session=session, id=id)
+    if update_wallet.operation == OperationEnum.DEPOSIT:
+        await session.execute(
+            update(Wallet)
+            .where(Wallet.id == id)
+            .values(
+                balance=wallet.balance + update_wallet.amount,
+                updated=update_wallet.updated,
+            )
+        )
+        await session.commit()
+        await session.refresh(wallet)
+        return wallet
+    if update_wallet.amount > wallet.balance:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Недостаточно средств на счету",
+        )
+    await session.execute(
+        update(Wallet)
+        .where(Wallet.id == id)
+        .values(
+            balance=wallet.balance - update_wallet.amount,
+            updated=update_wallet.updated,
+        )
+    )
+    await session.commit()
+    await session.refresh(wallet)
     return wallet
