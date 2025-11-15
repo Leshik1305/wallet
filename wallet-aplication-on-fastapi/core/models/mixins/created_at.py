7805c81de8dc287cd24c76c77d0a6004d7ab8dc1
Leshik1305 <realleshik1305@gmail.com>
2025-11-15 15:14:12 +0300

    create created_at mixin

---
+++ wallet-aplication-on-fastapi/core/models/mixins/created_at.py
@@ -0,0 +1,16 @@
+from datetime import datetime, timezone
+
+from sqlalchemy import func, DateTime
+from sqlalchemy.orm import Mapped, mapped_column
+
+
+def get_current_dt() -> datetime:
+    dt = datetime.now(tz=timezone.utc)
+    return dt.replace(tzinfo=None)
+
+
+class CreatedAtMixin:
+    created_at: Mapped[datetime] = mapped_column(DateTime,
+        default=get_current_dt,
+        server_default=func.now(),
+    )
