24e73f75bee9e4b860e46a4df8dc5f80cdb58db4
Leshik1305 <realleshik1305@gmail.com>
2025-11-15 18:19:22 +0300

    create crud get_wallet

--- wallet-aplication-on-fastapi/api/api_v1/crud/wallets.py
+++ wallet-aplication-on-fastapi/api/api_v1/crud/wallets.py
@@ -1,3 +1,7 @@
+from uuid import UUID
+
+from fastapi import HTTPException, status
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.models import Wallet
@@ -12,4 +16,18 @@ async def create_wallet(
     session.add(wallet)
     await session.commit()
     await session.refresh(wallet)
+    return wallet
+
+
+async def get_wallet(
+    session: AsyncSession,
+    id: UUID,
+) -> Wallet:
+    stmt = select(Wallet).where(Wallet.id == id, Wallet.is_active)
+    wallet = await session.scalar(stmt)
+    if not wallet:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Кошелек по данному ID не найден",
+        )
     return wallet
